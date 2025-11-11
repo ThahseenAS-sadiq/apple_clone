4158607748e060d22a60eec272908b030708f1e4
Thahseen AS <thahseen.sadiqas@gmail.com>
2025-11-11 22:29:52 +0530

    Add base path for Vite configuration
    Set the base path for the Vite application.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// ✅ Change this line — use your GitHub repo name here
 export default defineConfig({
   plugins: [react()],
+  base: '/apple_clone/', // 👈 Add this line
 })
